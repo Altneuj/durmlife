f97f273660659fce735e5b92f93106dde2db7423
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-04-24 15:35:15 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var mainPic = document.getElementById('main-bar-pic');
+
+document.getElementsByClassName('small-img')[0].addEventListener('click', function() {
+  mainPic.src= "https://www.projectshift.io/wp-content/uploads/2017/12/Untitled-design-4.png";
+});
+
+document.getElementsByClassName('small-img')[1].addEventListener('click', function() {
+  mainPic.src= "https://www.projectshift.io/wp-content/uploads/2017/12/Untitled-design-5.png";
+});
+
+document.getElementsByClassName('small-img')[2].addEventListener('click', function() {
+  mainPic.src= "https://www.projectshift.io/wp-content/uploads/2017/12/Untitled-design-6.png";
+});
